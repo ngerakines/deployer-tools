61f700b8f1a2eff0f9802f0f49375a7004d64ff9
Nick Gerakines <nick@gerakines.net>
2017-02-07 08:58:21 -0500

    Adding test mode.

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ func main() {
 
 func realMain(args []string) int {
 
-	var silent bool
+	var dry bool
+	var test bool
 	var mappingFile string
 	var eventFile string
 	var hostTemplate string
@@ -24,21 +25,25 @@ func realMain(args []string) int {
 		flags.PrintDefaults()
 	}
 
-	flags.BoolVar(&silent, "silent", true, "Operate silently with not output. Defaults to true.")
+	flags.BoolVar(&dry, "dry", false, "Prefixes all output with '#' characters.")
+	flags.BoolVar(&test, "test", false, "Use to test scripts.")
 	flags.StringVar(&mappingFile, "mapping", "", "The location of the mapping file.")
 	flags.StringVar(&eventFile, "event", "", "The location of the event file.")
 	flags.StringVar(&hostTemplate, "host-template", "", "The template used to build the host DNS entry.")
 
 	if err := flags.Parse(args); err != nil {
-		if silent == false {
-			flags.Usage()
-		}
+		flags.Usage()
 		return 1
 	}
 
 	if len(args) > 0 && args[0] == "help" {
 		flags.Usage()
 		return 1
+	}
+
+	if test == true {
+		fmt.Println("echo 'deployer-tools test'")
+		return 0
 	}
 
 	if mappingFile == "" || eventFile == "" {
@@ -62,18 +67,15 @@ func realMain(args []string) int {
 		return 1
 	}
 
-	// fmt.Printf("%# v\n", pretty.Formatter(mapping))
-	// fmt.Printf("%# v\n", pretty.Formatter(event))
-
 	switch event.Type {
 	case "service.update":
-		// fmt.Println("Updating services.")
 		if err = ServiceUpdate(mapping, event, hostTemplate); err != nil {
 			fmt.Println(err)
 			return 1
 		}
 	default:
 		fmt.Printf("Unknown event type '%s'.", event.Type)
+		return 1
 	}
 
 	return 0
